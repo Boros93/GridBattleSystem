176836e36153b10e5739c7e772f42d988e54e33d
Boros93 <andrea_sequenzia@yahoo.it>
2020-01-30 15:57:45 +0100

    Add instantiate tilemap

---
+++ Battle Project/Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    // Array 2D che tiene il riferimento ai prefab
+    GameObject[,] tilesOnMap;
+
+    [Header("Map Settings:")]
+    public int mapSizeX;
+    public int mapSizeY;
+    public GameObject tileContainer;
+
+    [Header("Prefab of Tiles:")]
+    public Tile[] prefabTiles;
+    // Per tenere il riferimento delle coordinate
+    public int[,] tiles;
+
+    public enum TileType
+    {
+        TILE_GRASS = 0,
+        TILE_WATER = 1,
+    }
+
+    private void Start()
+    {
+        GenerateMapInfo();
+        GenerateMapGraphics();
+    }
+
+    // Genera il riferimento alla mappa
+    private void GenerateMapInfo()
+    {
+        tiles = new int[mapSizeX, mapSizeY];
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                tiles[x, y] = (int)TileType.TILE_GRASS;
+            }
+        }
+
+        tiles[0,0] = (int)TileType.TILE_WATER;
+    }
+
+    private void GenerateMapGraphics()
+    {
+        tilesOnMap = new GameObject[mapSizeX, mapSizeY];
+        int index;
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                index = tiles[x, y];
+                GameObject newTile = Instantiate(prefabTiles[index].tilePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                newTile.transform.SetParent(tileContainer.transform);
+                tilesOnMap[x, y] = newTile;
+
+            }
+        }
+    }
+}
---
+++ Battle Project/Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tile
+{
+    public string tileName;
+    public GameObject tilePrefab;
+    // Set for special tile
+    public float movementCost = 1;
+    public bool isWalkable = true;
+}
